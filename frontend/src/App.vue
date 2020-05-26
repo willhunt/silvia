0d87af74ff0ed3e17328e4bd0045d83e65340667
Will Hunt <will.hunt.mail@gmail.com>
2020-05-26 09:29:40 +0100

    Prototype ready for testing on hardware

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import { eventBus } from '@/main'
 import AppNaviagtion from './components/AppNavigation'
 
@@ -34,12 +35,40 @@ export default {
   },
 
   created () {
+    //  Register eventbus methods:
+
     // Handle machine on/off globally
     eventBus.$on('toggleOnOff', () => {
       // Can send ajax request here
-
       this.machineOn = !this.machineOn
+
+      const axiosData = {
+        id: 1,
+        on: this.machineOn
+      }
+      const axiosConfig = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      axios.put('/api/v1/status/1', axiosData, axiosConfig)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => console.log(error))
     })
+
+    eventBus.$on('updateOnOff', () => {
+      axios.get('/api/v1/status/1')
+        .then(response => {
+          this.machineOn = Boolean(response.data.on)
+        })
+        .catch(error => console.log(error))
+    })
+
+    // Now fire event to check on/off status
+    // eventBus.$emit('updateOnOff')
+
   }
 
 }
--- frontend/src/components/AppNavigation.vue
+++ frontend/src/components/AppNavigation.vue
@@ -16,7 +16,7 @@
         </template>
       </v-list>
 
-      <a href="http://www.google.co.uk">
+      <a href="admin">
         <v-list-item class="primary darken-2" link style="position: absolute; bottom:0; width: 100%">
           <v-list-item-action>
             <v-icon>mdi-lock</v-icon>
@@ -42,7 +42,11 @@
       <v-toolbar-title class="hidden-sm-and-down">{{appTitle}}</v-toolbar-title>
 
       <v-spacer class="hidden-sm-and-down"></v-spacer>
-      <v-btn color="secondary" class="hidden-sm-and-down" to="/" @click="toggleOnOff">{{ machineOn ? "Off" : "On" }}</v-btn>
+      <!-- <v-btn color="secondary" class="hidden-sm-and-down" to="/" @click="toggleOnOff">{{ machineOn ? "Off" : "On" }}</v-btn> -->
+      <v-btn color="secondary" class="hidden-sm-and-down" to="/" @click="toggleOnOff">
+        <div v-if="machineOn"><v-icon color="success">mdi-power</v-icon></div>
+        <div v-else><v-icon>mdi-power</v-icon></div>
+      </v-btn>
     </v-app-bar>
   </span>
 
