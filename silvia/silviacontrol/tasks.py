33c5c1850e62a4d3335b935f80aff917a0ac0b96
Will Hunt <will.hunt.mail@gmail.com>
2020-06-04 12:05:07 +0100

    Machine in harwware mode

--- silvia/silviacontrol/tasks.py
+++ silvia/silviacontrol/tasks.py
@@ -8,8 +8,9 @@ from django.conf import settings
 from smbus import SMBus
 
 # I2C variables
-i2c_addr = 0x8
-i2c_bus = SMBus(1)  # Indicates /dev/ic2-1
+if settings.SIMULATE_MACHINE == False:
+    i2c_addr = 0x8
+    i2c_bus = SMBus(1)  # Indicates /dev/ic2-1
 
 @shared_task
 def async_get_response():
@@ -43,10 +44,11 @@ def async_power_machine(on):
     Inputs
         on: True = machine on, False = machine off [Bool]
     """
-    if on:
-        i2c_bus.write_byte(i2c_addr, 0x1) # switch it on
-    else:
-        i2c_bus.write_byte(i2c_addr, 0x0) # switch it off
+    if settings.SIMULATE_MACHINE == False:
+        if on:
+            i2c_bus.write_byte(i2c_addr, 0x1) # switch it on
+        else:
+            i2c_bus.write_byte(i2c_addr, 0x0) # switch it off
 
     debug_log("Celery machine on: %s" % on)
     status = StatusModel.objects.get(id=1)
