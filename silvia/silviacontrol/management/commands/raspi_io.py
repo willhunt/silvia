06caafeeb4010da963c5c9933c00b3207bc4279c
Will Hunt <will.hunt.mail@gmail.com>
2020-06-06 18:59:23 +0100

    corrected

--- silvia/silviacontrol/management/commands/raspi_io.py
+++ silvia/silviacontrol/management/commands/raspi_io.py
@@ -5,11 +5,11 @@ from silviacontrol.tasks import async_power_machine
 
 def trigger_celery_machine_on():
     # Add the code to create a new task here
-    async_power_machine(True)
+    async_power_machine.delay(True)
 
 def trigger_celery_machine_off():
     # Add the code to create a new task here
-    async_power_machine(False)
+    async_power_machine.delay(False)
 
 class Command(BaseCommand):
     help = 'Registers functions for GPIO'
@@ -18,7 +18,7 @@ class Command(BaseCommand):
         button = Button(7)
 
         button.when_pressed = trigger_celery_machine_on
-        # button.when_released = trigger_celery_machine_off
+        button.when_released = trigger_celery_machine_off
 
         # Wait for events
         self.stdout.write(self.style.SUCCESS('Waiting for button press'))
