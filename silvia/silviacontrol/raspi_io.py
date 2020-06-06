b09bc09ab76bf2e305589927e337bc15afa6a87b
Will Hunt <will.hunt.mail@gmail.com>
2020-06-06 17:56:08 +0100

    Script to detect button press

---
+++ silvia/silviacontrol/raspi_io.py
@@ -0,0 +1,19 @@
+from gpiozero import Button
+from signal import pause
+from .tasks import async_power_machine
+
+def trigger_celery_machine_on():
+    # Add the code to create a new task here
+    async_power_machine(True)
+
+def trigger_celery_machine_off():
+    # Add the code to create a new task here
+    async_power_machine(False)
+
+button = Button(7)
+
+button.when_pressed = trigger_celery_machine_on
+# button.when_released = trigger_celery_machine_off
+
+# Wait for events
+pause()
