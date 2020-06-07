ff7c1ad4e3e95db2ebd017b0ede0430a01c4d865
Will Hunt <will.hunt.mail@gmail.com>
2020-06-07 19:40:37 +0100

    Script to update files from github

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+gnome-terminal -e "sh -c 'git fetch --all; sleep 2; git reset --hard origin/master; sleep 1; \
+    chmod g+w silvia/db.sqlite3 silvia/celery.sqlite; sleep 1; \
+    chgrp server_group silvia/db.sqlite3 silvia/celery.sqlite; sh'"
+
+# gnome-terminal \
+# --tab -e "sh -c 'roscore; sh'" \
+# --tab -e "sh -c 'sleep 2; source devel/setup.bash; roslaunch myrobot_main main.launch; sh'" \
+# --tab -e "sh -c 'sleep 2; roslaunch myrobot_markers markers.launch; sh'" \
+# --tab -e "sh -c 'sleep 2; roslaunch myrobot_pick pick.launch; sh'"
