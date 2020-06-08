d42e6c527193589a7965d6275464039f6b95301a
Will Hunt <will.hunt.mail@gmail.com>
2020-06-08 09:33:10 +0100

    Changed

--- update.sh
+++ update.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-gnome-terminal -e "sh -c 'git fetch --all; sleep 2; git reset --hard origin/master; sleep 1; \
-    chmod g+w silvia/db.sqlite3 silvia/celery.sqlite; sleep 1; \
-    chgrp server_group silvia/db.sqlite3 silvia/celery.sqlite; sh'"
+# gnome-terminal -e "sh -c 'git fetch --all; sleep 2; git reset --hard origin/master; sleep 1; \
+#     chmod g+w silvia/db.sqlite3 silvia/celery.sqlite; sleep 1; \
+#     chgrp server_group silvia/db.sqlite3 silvia/celery.sqlite; sh'"
+
+git fetch --all
+sleep 2
+git reset --hard origin/master
+sleep 1
+chmod g+w silvia/db.sqlite3 silvia/celery.sqlite
+sleep 1
+chgrp server_group silvia/db.sqlite3 silvia/celery.sqlite
 
 # gnome-terminal \
 # --tab -e "sh -c 'roscore; sh'" \
