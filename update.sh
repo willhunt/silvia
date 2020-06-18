67a502ea76cb2a6d6cbb251cf81e78e18e0cf5f7
Will Hunt <will.hunt.mail@gmail.com>
2020-06-18 09:42:54 +0100

    Added carriage returns

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo -n "Fetching latest..."
 git fetch --all
 sleep 2
 git reset --hard origin/master
@@ -11,6 +10,6 @@ chgrp server_group silvia/db.sqlite3 silvia/celery.sqlite
 sleep 1
 chmod +x update.sh
 sleep 1
-echo -n "Restarting server..."
+echo -n "Restarting server... \c"
 sudo service apache2 restart
-echo -n "Done"
+echo -n "Done \c"
