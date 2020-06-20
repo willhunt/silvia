144a38e4ca0e0aa8d491fff2cb7f47f0e4b51c11
Will Hunt <will.hunt.mail@gmail.com>
2020-06-20 23:12:29 +0100

    Functionality to kill tasks on ctrl+c

--- silvia/background_tasks.sh
+++ silvia/background_tasks.sh
@@ -1,5 +1,11 @@
+# This will stop tasks when using Ctrl+C
+trap "kill 0" EXIT
+
 # Celery
 celery -A silvia worker &
 
 # Pi GPIO
 python manage.py raspi_interrupt &
+
+# Waits until process finishes (or Ctrl+c)
+wait
