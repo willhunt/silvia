3fa709c05bcf7216edf1b8670718d51ac402ed36
Will Hunt <will.hunt.mail@gmail.com>
2020-06-24 16:11:46 +0100

    Update

--- frontend/src/components/ScheduleEntry.vue
+++ frontend/src/components/ScheduleEntry.vue
@@ -17,6 +17,44 @@
           <v-switch color="secondary" :value="active" @change="toggleActive"></v-switch>
         </v-col>
       </v-row>
+      <!-- Time -->
+      <v-row v-if="showExtra" class="mx-2">
+        <v-col>
+          <v-dialog ref="dialog" v-model="dialogStart" persistent width="290px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                :value="start_time"
+                label="Start time"
+                prepend-icon="mdi-clock"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-card>
+              <v-time-picker v-if="dialogStart" :value="start_time" color="secondary" full-width></v-time-picker>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn text color="error" @click="dialogStart = false">Cancel</v-btn>
+                <v-btn text color="success" @click="updateStartTime($event)">OK</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-col>
+        <v-col>
+          <!-- <v-time-picker v-model="picker" color="secondary"></v-time-picker> -->
+        </v-col>
+      </v-row>
+      <!-- Actions -->
+      <v-card-actions >
+        <v-spacer></v-spacer>
+        <div v-if="showExtra">
+          <v-icon class="mx-2">mdi-content-save</v-icon>
+        </div>
+        <v-btn icon @click="showExtra = !showExtra" >
+          <v-icon>{{ showExtra ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </div>
 </template>
@@ -26,7 +64,9 @@ export default {
   name: 'ScheduleEntry',
   data: function () {
     return {
-      dayLetters: ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+      dayLetters: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+      showExtra: false,
+      dialogStart: false
     }
   },
   props: {
@@ -44,6 +84,9 @@ export default {
   methods: {
     toggleActive () {
       console.log('Toggle active')
+    },
+    updateStartTime (event) {
+      console.log('update time')
     }
   }
 }
