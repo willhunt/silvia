a78b362a45396a9ec1df186641fbc8b0b16fe5cc
Will Hunt <will.hunt.mail@gmail.com>
2020-06-24 17:24:37 +0100

    fixes

--- silvia/silviacontrol/display.py
+++ silvia/silviacontrol/display.py
@@ -3,23 +3,22 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-class SilviaDisplay(Adafruit_SSD1306):
+class SilviaDisplay(Adafruit_SSD1306.SSD1306_128_64):
 
     def __init__(self, i2c_address):
         super().__init__(rst=None, i2c_address=i2c_address)
         self.begin()
         self.clear()
         self.display()
-        
-        self.width = display.width
-        self.height = display.height
+    
         self.font = ImageFont.load_default()
         
     def welcome(self):
+        image = Image.new('1', (self.width, self.height))
         # Get drawing object to draw on image.
         draw = ImageDraw.Draw(image)
         # Draw a black filled box to clear the image.
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
         
         draw.text((2, 2),  'Hello',  font=self.font, fill=255)
         draw.text((2, 22), 'World!', font=self.font, fill=255)
