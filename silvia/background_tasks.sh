bfe493dcec7c354b9306ffa50070c983404cc4e7
Will Hunt <will.hunt.mail@gmail.com>
2020-07-04 22:14:58 +0100

    Clear queue

--- silvia/background_tasks.sh
+++ silvia/background_tasks.sh
@@ -2,7 +2,9 @@
 trap "kill 0" EXIT
 
 # Celery
+redis-cli flushall
 celery -A silvia worker &
+celery -A silvia beat --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Pi GPIO
 python manage.py raspi_interrupt &
