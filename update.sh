0e6da3c7bc7d7d8295d426fde5a3fcac2420df0c
Will Hunt <will.hunt.mail@gmail.com>
2020-08-03 16:59:39 +0100

    Added collectstatic

--- update.sh
+++ update.sh
@@ -12,6 +12,8 @@ chgrp server_group silvia/db.sqlite3 silvia/celery.sqlite
 sleep 1
 chmod +x update.sh
 sleep 1
+printf "Collecting Django static files..."
+python silvia/manage.py collectstatic --noinput
 printf "Restarting server...  "
 sudo service apache2 restart
 printf "Done  \n"
