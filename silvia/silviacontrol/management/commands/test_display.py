41f9c9ae29c7b288f866f6880992850da758e5fd
Will Hunt <will.hunt.mail@gmail.com>
2020-08-10 14:43:19 +0100

    Add test display management command

---
+++ silvia/silviacontrol/management/commands/test_display.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings as django_settings
+from silviacontrol.utils import debug_log
+
+class Command(BaseCommand):
+    help = 'Registers functions for GPIO interrupts on pi'
+
+    def handle(self, *args, **options):
+        if django_settings.SIMULATE_MACHINE == False:
+            from silviacontrol.display_cp import SilviaDisplay
+
+            display = SilviaDisplay(0x3C)
+            display.welcome()
+        else:
+            debug_log("Nothing to display in simulation mode")
