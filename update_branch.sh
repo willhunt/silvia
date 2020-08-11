4fe3d40be0737ec476e99da6e4468390e1410847
Will Hunt <will.hunt.mail@gmail.com>
2020-08-11 09:51:39 +0100

    update file changed

--- update_branch.sh
+++ update_branch.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 git fetch --all
-sleep 2
-git reset --hard origin/dev-schedule
-sleep 1
-git checkout dev-schedule
-sleep 1
+git reset --hard origin/serial-comms
+git checkout master
 chmod g+w silvia/db.sqlite3 silvia/celery.sqlite
-sleep 1
 chgrp server_group silvia/db.sqlite3 silvia/celery.sqlite
-sleep 1
-chmod +x update_dev_schedule.sh
-sleep 1
+chmod +x update_branch.sh
+printf "Collecting Django static files..."
+python silvia/manage.py collectstatic --noinput
 printf "Restarting server...  "
 sudo service apache2 restart
 printf "Done  \n"
