0e0d6308118597f7940fb615b325b844d7932872
Will Hunt <will.hunt.mail@gmail.com>
2020-08-17 12:26:12 +0100

    Build

--- silvia/silvia/settings.py
+++ silvia/silvia/settings.py
@@ -184,6 +184,6 @@ WHITENOISE_ROOT = os.path.join(STATIC_ROOT, 'silviacontrol/vue')
 
 # App in simulation mode or not
 # SIMULATE_MACHINE = True
-SIMULATE_MACHINE = True
+SIMULATE_MACHINE = False
 
 ARDUINO_COMMS = "i2c"  # "serial" or "i2c"
