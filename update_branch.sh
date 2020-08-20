e0b33588a5810616ee996fab46ea6a32347f4fda
Will Hunt <will.hunt.mail@gmail.com>
2020-08-20 12:20:49 +0100

    Untrack staticfiles, collect on local machine

--- update_branch.sh
+++ update_branch.sh
@@ -7,7 +7,7 @@ chmod g+w silvia/db.sqlite3 silvia/celery.sqlite
 chgrp server_group silvia/db.sqlite3 silvia/celery.sqlite
 chmod +x update_branch.sh
 printf "Collecting Django static files..."
-python silvia/manage.py collectstatic --noinput
+python silvia/manage.py collectstatic --noinput --clear
 printf "Restarting server...  "
 sudo service apache2 restart
 printf "Done  \n"
