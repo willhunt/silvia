4273de9863ff1f3ad83cfdce0ce875a643439e30
Will Hunt <will.hunt.mail@gmail.com>
2020-08-29 20:25:22 +0100

    fix

--- silvia/silviacontrol/urls.py
+++ silvia/silviacontrol/urls.py
@@ -10,13 +10,11 @@ router.register(r'status', views.StatusViewSet)
 router.register(r'response', views.ResponseViewSet, basename='response')
 router.register(r'session', views.SessionViewSet)
 router.register(r'schedule', views.ScheduleViewSet)
-# router.register(r'override', views.ManualControlViewSet, basename='override')
 
 urlpatterns = [
     # Rest API routing
     url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/override', views.ManualControlView.as_view()),
-    # url(r'^api/v1/autotune', views.AutoTuneView.as_view()),
     url(r'^api-auth/', include('rest_framework.urls')),
     # Django routing
     path('easter/', views.index, name='index'),
