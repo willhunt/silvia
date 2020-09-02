16e0d0b5b267599771a500fed5b9ab4b43289853
Will Hunt <will.hunt.mail@gmail.com>
2020-09-02 14:10:27 +0100

    New tatctic

--- silvia/silviacontrol/display_depreciated.py
+++ silvia/silviacontrol/display_depreciated.py
@@ -1,28 +1,21 @@
 from django.conf import settings as django_settings
-from silviacontrol.utils import debug_log
-from board import SCL, SDA
-import busio
-import board
-import adafruit_ssd1306
+import Adafruit_SSD1306
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import time
 
-class SilviaDisplay():
-    """
-    Display using newer Adafruit circuit python library
-    """
+class SilviaDisplay(Adafruit_SSD1306.SSD1306_128_64):
 
     def __init__(self, i2c_address):
-        self.i2c_address = i2c_address
+        super().__init__(rst=None, i2c_address=i2c_address)
+        self.begin()
+        self.clear()
+        self.display()
+    
+        # self.font = ImageFont.load_default()
         self.font_data = ImageFont.truetype(django_settings.STATIC_ROOT + '/silviacontrol/fonts/Roboto-Regular.ttf', 30)
         self.font_sub = ImageFont.truetype(django_settings.STATIC_ROOT + '/silviacontrol/fonts/Roboto-Regular.ttf', 12)
-        self.showBlank()
-
-    def getDisplay(self):
-        i2c = board.I2C()
-        return adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=self.i2c_address)
         
     def welcome(self):
         self.showWelcome()
@@ -30,20 +23,26 @@ class SilviaDisplay():
         self.showBlank()
 
     def showWelcome(self):
-        display = self.getDisplay()
         image = Image.open(django_settings.STATIC_ROOT + '/silviacontrol/display/silvia_logo_128x64_inverted.png') \
-                     .resize((display.width, display.height), Image.ANTIALIAS) \
+                     .resize((self.width, self.height), Image.ANTIALIAS) \
                      .convert('1')
-        display.image(image)
-        display.show()
+        self.image(image)
+        self.display()
 
     def showTemperature(self, T, T_set):
-        display = self.getDisplay()
-        image = Image.new('1', (display.width, display.height))
+        image = Image.new('1', (self.width, self.height))
         # Get drawing object to draw on image.
         drawing = ImageDraw.Draw(image)
+        # Draw a black filled box to clear the image.
+        drawing.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
+        
         padding_x = 2
         padding_y = 2
+
+        # draw.text((padding_x, padding_y),  'Temperature:',  font=self.font, fill=255)
+        # draw.text((padding_x + 13, padding_y),  "{:.1f}".format(T),  font=self.font, fill=255)
+        # draw.text((padding_x, padding_y + 30), 'Setpoint:', font=self.font, fill=255)
+        # draw.text((padding_x + 13, padding_y + 30),  "{:.1f}".format(T_set),  font=self.font, fill=255)
 
         drawing.text((padding_x, padding_y),
             "Temperature:",  font=self.font_sub, fill=255)
@@ -53,14 +52,10 @@ class SilviaDisplay():
         else:
             drawing.text((padding_x + 4, 22),
                 "{0:.0f}{1}C".format(T, u'\N{DEGREE SIGN}'),  font=self.font_data, fill=255)
-            
+                
         drawing.text((85, 35),
             "[{0:.0f}{1}C]".format(T_set, u'\N{DEGREE SIGN}'), font=self.font_sub, fill=255)
 
-        display.image(image)
-        display.show()
-
     def showBlank(self):
-        display = self.getDisplay()
-        display.fill(0)
-        display.show()
+        self.clear()
+        self.display()
