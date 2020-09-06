41fbdc1ef535c3aea4e7d342ac225c22e007b422
Will Hunt <will.hunt.mail@gmail.com>
2020-09-06 22:22:49 +0100

    Add arduino

--- update_branch.sh
+++ update_branch.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
 branch=$1
+arduino=${2:-0}
 
 git fetch --all
 git reset --hard origin/$branch
 git checkout $branch
 printf "Collecting Django static files..."
 python silvia/manage.py collectstatic --noinput --clear
+
+if [ $arduino -eq 1 ]
+then
+    printf "Compile Arduino code..."
+    arduino-cli compile --fqbn arduino:avr:uno silvia_arduino
+fi
+
 printf "Restarting server...  "
 sudo service apache2 restart
+
+if [ $arduino -eq 1 ]
+then
+    printf "Upload to Arduino..."
+    arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno silvia_arduino --verbose
+fi
+
 printf "Done  \n"
