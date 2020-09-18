d847d44b423aee401937f660e74721c7b3579bbb
Will Hunt <will.hunt.mail@gmail.com>
2020-09-18 09:32:05 +0100

    Stop processes in update

--- update_branch.sh
+++ update_branch.sh
@@ -3,26 +3,28 @@
 branch=$1
 arduino=${2:-0}
 
+printf "Stopping background processes...  "
+sudo supervisorctl stop all
+
 git fetch --all
 git reset --hard origin/$branch
 git checkout $branch
+
+printf "Collecting Django static files..."
+python silvia/manage.py collectstatic --noinput --clear
 
 if [ $arduino -eq 1 ]
 then
     printf "Compile Arduino code..."
     arduino-cli compile --fqbn arduino:avr:uno silvia_arduino
-fi
-
-printf "Collecting Django static files..."
-python silvia/manage.py collectstatic --noinput --clear
-
-printf "Restarting server...  "
-sudo service apache2 restart
-
-if [ $arduino -eq 1 ]
-then
     printf "Upload to Arduino..."
     arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno silvia_arduino --verbose
 fi
+
+printf "Restarting background processes...  "
+sudo supervisorctl start all
+
+printf "Restarting server...  "
+sudo service apache2 restart
 
 printf "Done  \n"
